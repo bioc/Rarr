79b93a21b14e25e317030423a3b1ed460875d552
Hugo Gruson <git@hugogruson.fr>
2026-09-16 15:22:51 +0200

    Update EMBL test bucket to NetApp

--- tests/testthat/test-s3.R
+++ tests/testthat/test-s3.R
@@ -12,20 +12,18 @@ test_that("Anonymous S3 access is detected properly", {
 
 test_that("Authenticated S3 access works", {
   skip_if_offline("s3.embl.de")
-  skip_on_bioc()
-  skip_on_ci()
 
   withr::with_envvar(
     c(
-      "AWS_ACCESS_KEY_ID" = "bYUBYVg1AsEreuDgtg5K",
-      "AWS_SECRET_ACCESS_KEY" = "r8FrLXc9dseD6V1P3htsu7ZBzP7Gszsd3sM1G4KX"
+      "AWS_ACCESS_KEY_ID" = "NFXMU48714DJHNGTB825",
+      "AWS_SECRET_ACCESS_KEY" = "FZ0_p4_NrrB_fnm_e1DEEP7g3772I9uP3igu5D_C"
     ),
     {
       expect_snapshot(zarr_overview(
-        "https://s3.embl.de/rarr-testing/bzip2.zarr"
+        "https://huber.buckets.embl.de/rarr-testing/bzip2.zarr"
       ))
       expect_snapshot(read_zarr_array(
-        "https://s3.embl.de/rarr-testing/bzip2.zarr"
+        "https://huber.buckets.embl.de/rarr-testing/bzip2.zarr"
       ))
     }
   )
@@ -33,12 +31,10 @@ test_that("Authenticated S3 access works", {
 
 test_that("Denied access errors return clear error messages", {
   skip_if_offline("s3.embl.de")
-  skip_on_bioc()
-  skip_on_ci()
 
   # This is a real zarr store, but we don't have access
   expect_error(
-    zarr_overview("https://s3.embl.de/rarr-testing/lzma.zarr"),
+    zarr_overview("https://huber.buckets.embl.de/rarr-testing/lzma.zarr"),
     "Denied"
   )
 })
